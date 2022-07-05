6fa00ee0b1c91440aa60594436706572c1bee143
capitana-roja <105622368+capitana-roja@users.noreply.github.com>
2022-07-05 13:29:46 -0400

    Update data_dictionary.php

--- proficient/data_dictionary.php
+++ proficient/data_dictionary.php
@@ -27,19 +27,6 @@
 				<iframe class="pdf_location" src="https://cdn.atriumhealth.org/-/media/wakeforest/ctsi/files/services/data/redcap/basic-project-actions/download-the-data-dictionary.pdf?rev=bb584e3b10cf4195a324697543fb1800&hash=DF9B25E275381CE4100450596253C887" width="1000px" height="1200px"></iframe>
 				<iframe class="pdf_location" src="https://ws.engr.illinois.edu/sitemanager/getfile.asp?id=1365" width="1000px" height="1200px"></iframe>			
 			</div>
-			<!--Last section of content-->
-			<div class="remember">
-				<a id="thingstoremember">
-					<h2>Things to Remember</h2>
-				</a>
-				<hr>
-				<ol>
-					<li>
-					<li>
-					<li>
-					<li>
-				</ol>
-			</div>
 		</div>
 		<script src="../script.js"></script>
 	</body>
