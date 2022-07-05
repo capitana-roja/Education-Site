553db0b1c190634a5b9b09a5d8668b1ed59796ac
capitana-roja <105622368+capitana-roja@users.noreply.github.com>
2022-07-05 14:05:48 -0400

    Update project_revision_history.php

--- beginner/project_revision_history.php
+++ beginner/project_revision_history.php
@@ -26,67 +26,8 @@
 				<!--PDF content-->
 				<iframe class="pdf_location" src="beginner_resources/project_revision_history.pdf"></iframe>
 			</div>
-			<!--Last section of content-->
-			<div class="remember">
-				<a id="thingstoremember">
-					<h2>Things to Remember</h2>
-				</a>
-				<hr>
-				<ol>
-					<li>
-					<li>
-					<li>
-					<li>
-				</ol>
-			</div>
-		<script src="../script.js"></script>
-	</body>
-	<footer></footer>
-</html>
-
-		<!--<div class="body_text">
-			<a id="purpose">
-				<div class="purpose">
-					<p> are used in REDCap to</p>
-				</div>
-			</a>
-			<div style="color:black;">
-				<br>
-				<a id="procedure">
-					<h2 style="color:#b22227;font-weight:normal;">Procedure</h2>
-				</a>
-				<hr>
-				<p></p>
-			</div>
-			<br>
-			<div style="color:black;">
-				<a id="examples">
-					<h2 style="color:#b22227;font-weight:normal;">Examples</h2>
-				</a>
-				<hr>
-				<p></p>
-				<ul style="list-style-type: upper-roman; padding-left: 25px;">
-					<li>
-					<li>
-					<li>
-					<li>
-				</ul>
-			</div>
-			<br>
-			<div style="color:black;">
-				<a id="thingstoremember">
-					<h2 style="color:#b22227;font-weight:normal;">Things to Remember</h2>
-				</a>
-				<hr>
-				<ol style="padding-left: 20px;">
-					<li>
-					<li>
-					<li>
-					<li>
-				</ol>
-			</div>
 		</div>
 		<script src="../script.js"></script>
 	</body>
 	<footer></footer>
-</html>-->		
+</html>
