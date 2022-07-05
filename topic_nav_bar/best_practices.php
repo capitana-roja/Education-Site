54d57749bd071d05dbd0992bc9ffd48c3a77d000
capitana-roja <105622368+capitana-roja@users.noreply.github.com>
2022-07-05 12:58:30 -0400

    Update best_practices.php

--- topic_nav_bar/best_practices.php
+++ topic_nav_bar/best_practices.php
@@ -24,7 +24,7 @@
 		<div class="topic_pdf">
 			<div class="embedded_pdf">
 				<!--PDF content-->
-				<iframe class="pdf_location" src="REDCap_BestPractices.pdf" width="1000px" height="1200px"></iframe>
+				<iframe class="pdf_location" src="../resources/topic_nav_bar/REDCap_BestPractices.pdf" width="1000px" height="1200px"></iframe>
 			</div>
 			<!--Javascript-->
 			<script src="../script.js"></script>
