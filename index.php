05510d267aedca52966f4823b383081b2b3a8261
capitana-roja <105622368+capitana-roja@users.noreply.github.com>
2022-07-07 14:41:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,3 @@
 <?php echo file_get_contents("topic_navigation_bar.html"); ?> 
 <?php echo file_get_contents("body.html"); ?> 
 <?php echo file_get_contents("footer.html"); ?>
-<p>Copyright © AHWFBMC <?php echo date("Y"); ?> This website was developed by Atrium Health & Wake Forest University School of Medicine. Its contents are solely the responsibility of the authors and do not necessarily represent the official views of the institution</p>
