c44d0a7ced801b4477cbf487b0aa9df1e1144693
capitana-roja <105622368+capitana-roja@users.noreply.github.com>
2022-07-07 15:04:56 -0400

    Update overview.php

--- beginner/overview.php
+++ beginner/overview.php
@@ -24,7 +24,7 @@
 		<div class="topic_pdf">
 			<div class="embedded_pdf">
 				<!--PDF content-->
-				<iframe class="pdf_location" src="resources/beginner/Overview_Guide_REDCap.pdf"></iframe>
+				<iframe class="pdf_location" src="../resources/beginner/Overview_Guide_REDCap.pdf"></iframe>
 			</div>
 		</div>
 		<script src="../script.js"></script>
