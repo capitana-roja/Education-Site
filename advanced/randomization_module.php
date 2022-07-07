40b565ed13f0c437c1b57bb3dde6ce4ca8f036b3
capitana-roja <105622368+capitana-roja@users.noreply.github.com>
2022-07-07 16:06:43 -0400

    Update randomization_module.php

--- advanced/randomization_module.php
+++ advanced/randomization_module.php
@@ -24,7 +24,7 @@
 		<div class="topic_pdf">
 			<div class="embedded_pdf">
 				<!--PDF content-->
-				<iframe class="pdf_location" src="resources/advanced/REDCap_Randomization.pdf" width="1000px" height="1200px"></iframe>
+				<iframe class="pdf_location" src="../resources/advanced/REDCap_Randomization.pdf" width="1000px" height="1200px"></iframe>
 			</div>
 			<!--Last section of content-->
 			<div class="remember">
