e8b0f86a0994bfda6d4779510736c730c03b2256
capitana-roja <105622368+capitana-roja@users.noreply.github.com>
2022-07-07 14:00:18 -0400

    Update add_edit_records.php

--- beginner/add_edit_records.php
+++ beginner/add_edit_records.php
@@ -24,7 +24,7 @@
 		<div class="topic_pdf">
 			<div class="embedded_pdf">
 				<!--PDF content-->
-				<iframe class="pdf_location" src="beginner_resources/Add_Edit_Records_Guide_REDCap.pdf"></iframe>
+				<iframe class="pdf_location" src="../resources/beginner/Add_Edit_Records_Guide_REDCap.pdf"></iframe>
 			</div>
 		<script src="../script.js"></script>
 	</body>
