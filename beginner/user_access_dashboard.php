fbca80de331c813508348b8fec760ae3e04b09ee
capitana-roja <105622368+capitana-roja@users.noreply.github.com>
2022-07-07 15:10:20 -0400

    Update user_access_dashboard.php

--- beginner/user_access_dashboard.php
+++ beginner/user_access_dashboard.php
@@ -24,7 +24,7 @@
 		<div class="topic_pdf">
 			<div class="embedded_pdf">
 				<!--PDF content-->
-				<iframe class="pdf_location" src="resources/beginner/User_Access_Dashboard_Guide_REDCap.pdf"></iframe>
+				<iframe class="pdf_location" src="../resources/beginner/User_Access_Dashboard_Guide_REDCap.pdf"></iframe>
 			</div>
 		</div>
 		<script src="../script.js"></script>
