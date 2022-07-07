bedfda7e47c3e889347c3abb7cd84cf4b74584ab
capitana-roja <105622368+capitana-roja@users.noreply.github.com>
2022-07-07 15:03:37 -0400

    Update data_comparison_tool.php

--- beginner/data_comparison_tool.php
+++ beginner/data_comparison_tool.php
@@ -24,7 +24,7 @@
 		<div class="topic_pdf">
 			<div class="embedded_pdf">
 				<!--PDF content-->
-				<iframe class="pdf_location" src="resources/beginner/data_comparison_tool.pdf"></iframe>
+				<iframe class="pdf_location" src="../resources/beginner/data_comparison_tool.pdf"></iframe>
 			</div>
 			<!--Javascript-->
 			<script src="../script.js"></script>
