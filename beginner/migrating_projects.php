50eacc22405391ea86b09d508b44095e9991fbcc
capitana-roja <105622368+capitana-roja@users.noreply.github.com>
2022-07-07 14:15:28 -0400

    Update migrating_projects.php

--- beginner/migrating_projects.php
+++ beginner/migrating_projects.php
@@ -24,7 +24,7 @@
 		<div class="topic_pdf">
 			<div class="embedded_pdf">
 				<!--PDF content-->
-				<iframe class="pdf_location" src="migrating_projects.pdf"</iframe>
+				<iframe class="pdf_location" src="resources/beginner/migrating_projects.pdf"</iframe>
 			</div>
 		<script src="../script.js"></script>
 		</div>
