1dca97e6176be77a5a02fb87993ce03be475d1db
tayjsmit <tayjsmit@wakehealth.edu>
2023-09-05 13:08:30 -0400

    Take nav out of container

--- beginner/add_edit_records.php
+++ beginner/add_edit_records.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php echo file_get_contents("../head_links_sub.html"); ?>		
+		<?php echo file_get_contents("../head_links_sub.html"); ?>	
 		<meta name="keywords" content="REDCap, training, education">
 		<meta name=”description” content=”This page educates REDCap users on adding and editing records”>
 		<title>Add/Edit Records</title>
@@ -9,28 +9,21 @@
 	</head>
 	<body>
 		<!--Navigation bars-->
+		<?php echo file_get_contents("../main_navigation_bar.html"); ?> 
 		<div class="container-fluid">
-			<?php echo file_get_contents("../main_navigation_bar.html"); ?> 
 			<?php echo file_get_contents("../topic_navigation_bar_sub.html"); ?>
 		
 			<!--Button back to top-->
 			<?php echo file_get_contents("../top_button.html"); ?>
-			<br>
-			<br>
-			<br>
-			<br>
+			
 			<!--Title-->
 			<h1 class="title_pdf">Add/Edit Records</h1>
 			<!--Content-->
-		
-		
 			<!--PDF content-->
 			<div class="container">
-
-			
-			<div class="card embed-responsive embed-responsive-1x1 justify-content-center">
-				<iframe class="embed-responsive-item" height='800' src="../resources/beginner/Add_Edit_Records_Guide_REDCap.pdf" allowfullscreen></iframe>
-			</div>
+				<div class="card embed-responsive embed-responsive-1x1 justify-content-center">
+					<iframe class="embed-responsive-item" height='800' src="../resources/beginner/Add_Edit_Records_Guide_REDCap.pdf" allowfullscreen></iframe>
+				</div>
 			</div>
 		</div>
 		<script src="../script.js"></script>
