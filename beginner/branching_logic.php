68490befcb368c16624841f076893afead1ae7bf
tayjsmit <tayjsmit@wakehealth.edu>
2023-09-26 14:25:41 -0400

    Add boostrap to beginner/branching_logic

--- beginner/branching_logic.php
+++ beginner/branching_logic.php
@@ -6,27 +6,25 @@
 		<meta name=”description” content=”This page educates REDCap users on branching logic”>
 		<title>Branching Logic</title>
 	</head>
-	
 	<body>
 		<!--Navigation bars-->
-		<div>
-			<?php echo file_get_contents("../main_navigation_bar_sub.html"); ?> 
+		<?php echo file_get_contents("../main_navigation_bar.html"); ?> 
+		<div class="container-fluid">
 			<?php echo file_get_contents("../topic_navigation_bar_sub.html"); ?>
-		</div>
-		<!--Button back to top-->
-		<?php echo file_get_contents("../top_button.html"); ?> 
-		<br>
-		<br>
-		<br>
-		<br>
-		<!--Title-->
-		<h1 class="title_pdf">Branching Logic</h1>
-		<!--Content-->
-		<div class="topic_pdf">
-			<div class="embedded_pdf">
-				<!--PDF content-->
-				<iframe class="pdf_location" src="https://www.ctsi.ufl.edu/wordpress/files/2017/06/Branching-Logic-in-REDCap-%E2%80%93-How.pdf"></iframe>
+		
+			<!--Button back to top-->
+			<?php echo file_get_contents("../top_button.html"); ?>
+			
+			<!--Title-->
+			<h1 class="title_pdf">Branching Logic</h1>
+			<!--Content-->
+			<!--PDF content-->
+			<div class="container">
+				<div class="card embed-responsive embed-responsive-1x1 justify-content-center">
+					<iframe class="embed-responsive-item" height='800' src="https://www.ctsi.ufl.edu/wordpress/files/2017/06/Branching-Logic-in-REDCap-%E2%80%93-How.pdf" allowfullscreen></iframe>
+				</div>
 			</div>
+		</div>
 		<script src="../script.js"></script>
 	</body>
 	<footer><?php echo file_get_contents("../footer.html"); ?></footer>
